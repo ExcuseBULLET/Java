f2721e067b553adc007b4accf6ba8a74ac58e0f9
Aryan Raj Singh <142614524+ExcuseBULLET@users.noreply.github.com>
2024-08-24 11:50:26 +0530

    Add files via upload

---
+++ JAVA/Conditional/practice1.java
@@ -0,0 +1,17 @@
+package Conditional;
+import java.util.Scanner;
+public class practice1{
+    public static void main(String agrs[]){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        n=sc.nextInt();
+
+        if(n%2==0){
+            System.out.println("The Number is Even");
+        }
+        else{
+            System.out.println("The Number is Odd");
+        }
+    }
+}
